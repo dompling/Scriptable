de82d765a5e465811c7f68d4bf5e8a5814b2d9a3
dompling <374779789@qq.com>
2020-10-20 15:12:05 +0800

    更新安装脚本

--- 2YaInstall.js
+++ 2YaInstall.js
@@ -2,12 +2,21 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: brown; icon-glyph: download;
 
-// 安装引入 Env 包：https://raw.githubusercontent.com/GideonSenku/Scriptable/master/Env.scriptable
 const scripts = [
   {
     moduleName: "Env",
     url:
       "https://raw.githubusercontent.com/GideonSenku/Scriptable/master/Env.js",
+  },
+  {
+    moduleName: "ZhihuMonitor",
+    url:
+      "https://raw.githubusercontent.com/evilbutcher/Scriptables/master/ZhihuMonitor.js",
+  },
+  {
+    moduleName: "2YaInstall",
+    url:
+      "https://raw.githubusercontent.com/dompling/Scriptable/master/2YaInstall.js",
   },
   {
     moduleName: "Calendar",
@@ -21,56 +30,59 @@ const scripts = [
   },
 ];
 
-const currentDate = new Date();
-const request = new Request("");
-const files = FileManager.iCloud();
-const dict = files.documentsDirectory();
-files.isDirectory(`${dict}/Env`) ? `` : files.createDirectory(`${dict}/Env`);
-const defaultHeaders = {
-  Accept: "*/*",
-  "Content-Type": "application/json",
-};
-const initFile = (fileName) => {
-  const hasSuffix = fileName.lastIndexOf(".") + 1;
-  return !hasSuffix ? `${fileName}.js` : fileName;
-};
+class YaYaInstall {
+  constructor() {
+    this.request = new Request("");
+    this.files = FileManager.iCloud();
+    this.dict = this.files.documentsDirectory();
+    this.defaultHeaders = {
+      Accept: "*/*",
+      "Content-Type": "application/json",
+    };
+  }
 
-const write = (fileName, content) => {
-  let file = initFile(fileName);
-  const filePath = `${dict}/${file}`;
-  FileManager.iCloud().writeString(filePath, content);
-  return true;
-};
-
-const getStr = async ({ url, headers = {} }, callback = () => {}) => {
-  request.url = url;
-  request.method = "GET";
-  request.headers = {
-    ...headers,
-    ...defaultHeaders,
+  initFile = (fileName) => {
+    const hasSuffix = fileName.lastIndexOf(".") + 1;
+    return !hasSuffix ? `${fileName}.js` : fileName;
   };
-  const data = await request.loadString();
-  callback(request.response, data);
-  return data;
-};
 
-const getFile = async ({ moduleName, url }) => {
-  console.log(`开始下载文件: 🌝 ${moduleName}`);
-  const header = `// Variables used by Scriptable.
+  write = (fileName, content) => {
+    let file = this.initFile(fileName);
+    const filePath = `${this.dict}/${file}`;
+    FileManager.iCloud().writeString(filePath, content);
+    return true;
+  };
+
+  getStr = async ({ url, headers = {} }, callback = () => {}) => {
+    this.request.url = url;
+    this.request.method = "GET";
+    this.request.headers = {
+      ...headers,
+      ...this.defaultHeaders,
+    };
+    const data = await this.request.loadString();
+    callback(this.request.response, data);
+    return data;
+  };
+
+  getFile = async ({ moduleName, url }) => {
+    console.log(`开始下载文件: 🌝 ${moduleName}`);
+    const header = `// Variables used by Scriptable.
   // These must be at the very top of the file. Do not edit.
   // icon-color: deep-gray; icon-glyph: file-code;\n`;
-  const content = await getStr({ url });
-  console.log(content);
-  const fileHeader = content.includes("icon-color") ? `` : header;
-  write(`${moduleName}`, `${fileHeader}${content}`);
-  console.log(`文件下载完成: 🌚 ${moduleName}`);
-};
+    const content = await this.getStr({ url });
+    console.log(content);
+    const fileHeader = content.includes("icon-color") ? `` : header;
+    this.write(`${moduleName}`, `${fileHeader}${content}`);
+    console.log(`文件下载完成: 🌚 ${moduleName}`);
+  };
 
-function update() {
-  console.log("🔔更新脚本开始!");
-  scripts.forEach(async (script) => {
-    await getFile(script);
-  });
-  console.log("🔔更新脚本结束!");
+  install = () => {
+    console.log("🔔更新脚本开始!");
+    scripts.forEach(async (script) => {
+      await this.getFile(script);
+    });
+    console.log("🔔更新脚本结束!");
+  };
 }
-update();
+new YaYaInstall().install();
