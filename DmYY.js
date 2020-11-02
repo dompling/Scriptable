2c985a18c0372ee18079dc6c73f4f77a38f41966
dompling <374779789@qq.com>
2020-11-02 14:22:31 +0800

    京东豆

---
+++ DmYY.js
@@ -0,0 +1,398 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: teal; icon-glyph: map-pin;
+
+/*
+ * Author: 2Ya
+ * Github: https://github.com/dompling
+ */
+
+class DmYY {
+  constructor() {
+    this.textStyle = this.mode
+      ? this._textFormat.light
+      : this._textFormat.defaultText;
+    if (this.blurBackground) {
+      if (typeof this.blurBackground === "number") {
+        this.backgroundOpacity = this.blurBackground;
+      } else {
+        this.backgroundOpacity = this.mode ? 0.7 : 0.4;
+      }
+    }
+  }
+  defaultCacheData = {};
+  prefix = "boxjs.net";
+  blurBackground = true;
+  imageBackground = true;
+  forceImageUpdate = false;
+  cacheBackgroundName = "2Ya-img";
+  mode = Device.isUsingDarkAppearance();
+  widgetSize = config.runsInWidget ? config.widgetFamily : "large";
+
+  defaultHeaders = {
+    Accept: "*/*",
+    "Content-Type": "application/json",
+  };
+
+  files = FileManager.local();
+
+  _textFormat = {
+    // Set the default font and color.
+    defaultText: { size: 14, color: "ffffff", font: "regular" }, // 默认字体颜色
+    light: { size: 14, color: "D0D3D4", font: "light" }, // 夜间字体颜色
+    title: { size: 16, color: "ff651a", font: "semibold" },
+    hot: { size: 20, color: "ffffff", font: "semibold" },
+    more: { size: 14, color: "ffffff", font: "regular" },
+  };
+
+  skinColor = {
+    defaultColor: {
+      color: [new Color("#a18cd1"), new Color("#fbc2eb")],
+      position: [0.0, 1.0],
+    },
+    night: {
+      color: [new Color("#030079"), new Color("#000000")],
+      position: [0.0, 1.0],
+    },
+  };
+
+  getRequest = (url = "") => {
+    return new Request(url);
+  };
+
+  http = async (options = { headers: {}, url: "" }, type = "JSON") => {
+    try {
+      let request;
+      if (type !== "IMG") {
+        request = this.getRequest();
+        Object.keys(options).forEach((key) => {
+          request[key] = options[key];
+        });
+        request.headers = { ...this.defaultHeaders, ...options.headers };
+      } else {
+        request = this.getRequest(options.url);
+        return await request.loadImage();
+      }
+      if (type === "JSON") {
+        return await request.loadJSON();
+      }
+      if (type === "STRING") {
+        return await request.loadString();
+      }
+      return await request.loadJSON();
+    } catch (e) {
+      console.log("error:" + e);
+    }
+  };
+
+  $request = {
+    get: async (url = "", options, type = "JSON") => {
+      const params = { url, ...options, method: "GET" };
+      let _type = type;
+      if (typeof options === "string") _type = options;
+      return await this.http(params, _type);
+    },
+    post: async (url = "", options, type = "JSON") => {
+      const params = { url, ...options, method: "POST" };
+      let _type = type;
+      if (typeof options === "string") _type = options;
+      return await this.http(params, _type);
+    },
+  };
+
+  getCache = async (key) => {
+    try {
+      const url = `http://${this.prefix}/query/boxdata`;
+      const boxdata = await this.$request.get(url);
+      let data;
+      const cacheKeys = Object.keys(this.defaultCacheData);
+      if (cacheKeys.length) {
+        data = {};
+        cacheKeys.forEach((params) => {
+          const datasKey = `${key}.${params}`;
+          const dataValue = boxdata.datas[datasKey];
+          if (dataValue) {
+            data[params] = dataValue;
+          }
+        });
+      } else {
+        const cacheValue = boxdata.datas[key];
+        if (cacheValue) {
+          data = this.transforJSON(cacheValue);
+        }
+      }
+      return data;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  };
+
+  transforJSON = (str) => {
+    if (typeof str == "string") {
+      try {
+        return JSON.parse(str);
+      } catch (e) {
+        console.log(e);
+        return str;
+      }
+    }
+    console.log("It is not a string!");
+  };
+
+  // 给图片加透明遮罩
+  setShadowImage = async (img, opacity) => {
+    if (!opacity) return img;
+    let ctx = new DrawContext();
+    // 获取图片的尺寸
+    ctx.size = img.size;
+    ctx.drawImageInRect(
+      img,
+      new Rect(0, 0, img.size["width"], img.size["height"])
+    );
+    ctx.setFillColor(new Color("#000000", opacity));
+    ctx.fillRect(new Rect(0, 0, img.size["width"], img.size["height"]));
+    return await ctx.getImage();
+  };
+
+  setWidgetBackGround = async (widget) => {
+    if (this.imageBackground) {
+      const path = this.getCacheFilepath();
+      const exists = this.verifyFilePath(path);
+      if (exists && (config.runsInWidget || !this.forceImageUpdate)) {
+        // 文件存在，读取缓存，设置背景
+        const image = this.files.readImage(path);
+        // 设置图片模糊程度
+        widget.backgroundImage = await this.setShadowImage(
+          image,
+          this.backgroundOpacity
+        );
+      } else if (!exists && config.runsInWidget) {
+        // 默认背景色
+        widget.backgroundColor = Color.gray();
+      } else {
+        // 设置选择图片背景
+        widget.backgroundImage = await this.chooseImgAndCache(path);
+      }
+    } else {
+      // 系统配置背景色
+      let gradient = new LinearGradient();
+      let gradientSettings = this.mode
+        ? this.skinColor.night
+        : this.skinColor.defaultColor;
+      gradient.colors = gradientSettings.color();
+      gradient.locations = gradientSettings.position();
+      widget.backgroundGradient = gradient;
+    }
+    return widget;
+  };
+
+  // 选择图片并缓存
+  chooseImgAndCache = async (path) => {
+    const photoLibrary = await Photos.fromLibrary();
+    this.files.writeImage(path, photoLibrary);
+    return photoLibrary;
+  };
+
+  // 验证文件路径
+  verifyFilePath = (path) => {
+    return this.files.fileExists(path);
+  };
+
+  // 获取缓存本地路径
+  getCacheFilepath = (cacheName = this.cacheBackgroundName) => {
+    return this.files.joinPath(this.files.documentsDirectory(), cacheName);
+  };
+
+  // 设置线行块儿元素
+  drawVerticalLine(color, height, width = 2) {
+    let draw = new DrawContext();
+    draw.opaque = false;
+    draw.respectScreenScale = true;
+    draw.size = new Size(width, height);
+
+    let barPath = new Path();
+    // const barHeight = height;
+    barPath.addRoundedRect(
+      new Rect(0, 0, height, height),
+      width / 2,
+      width / 2
+    );
+    draw.addPath(barPath);
+    draw.setFillColor(color);
+    draw.fillPath();
+    return draw.getImage();
+  }
+
+  setMore = async (headerBody, more) => {
+    headerBody.addSpacer(170);
+    // 右边更多
+    const headerRight = headerBody.addStack();
+    headerRight.url = more.url;
+    headerRight.setPadding(1, 10, 1, 10);
+    headerRight.cornerRadius = 10;
+    headerRight.backgroundColor = new Color(more.bgColor || "#fff", 0.5);
+    this.provideText("更多", headerRight, more.fontStyle);
+    widget.addSpacer(10);
+  };
+
+  // 设置头部信息
+  setHeader = async (
+    widget,
+    { icon = "", title = "", size = new Size(14, 16) },
+    more = false
+  ) => {
+    const headerBody = widget.addStack();
+    headerBody.centerAlignContent();
+    // 左边内容
+    const headerLeft = headerBody.addStack();
+    const iconImg = await this.$request.get(icon, "IMG");
+    let _icon = headerLeft.addImage(iconImg);
+    _icon.imageSize = size;
+    _icon.cornerRadius = 4;
+    headerLeft.addSpacer(5);
+    this.provideText(title, headerLeft);
+    if (more) {
+      if (typeof more === "function") {
+        await more(headerBody);
+      } else {
+        await this.setMore(headerBody, more);
+      }
+    }
+    return widget;
+  };
+
+  // 显示一行的字
+  setCellText = (text, cell, format = this.textFormat, prefixColor = true) => {
+    if (prefixColor) {
+      if (typeof prefixColor === "boolean") prefixColor = this.randomHexColor();
+      let tomorrowLine = cell.addImage(
+        this.drawVerticalLine(new Color(prefixColor, 0.8), 12)
+      );
+      tomorrowLine.imageSize = new Size(3, 28);
+    }
+    cell.addSpacer(5);
+    return this.provideText(text, cell, format);
+  };
+
+  // 生成随机色
+  randomHexColor() {
+    var hex = Math.floor(Math.random() * 16777216).toString(16); //生成ffffff以内16进制数
+    while (hex.length < 6) {
+      //while循环判断hex位数，少于6位前面加0凑够6位
+      hex = "0" + hex;
+    }
+    return hex; //返回‘#’开头16进制颜色
+  }
+
+  // 显示文字
+  provideText = (string, container, format = this.textStyle) => {
+    const textItem = container.addText(string);
+    const textFont = format.font;
+    const textSize = format.size;
+    const textColor = format.color;
+
+    textItem.font = this.provideFont(textFont, textSize);
+    textItem.textColor = new Color(textColor);
+    return textItem;
+  };
+
+  // 文字样式
+  provideFont(fontName, fontSize) {
+    const fontGenerator = {
+      ultralight: function () {
+        return Font.ultraLightSystemFont(fontSize);
+      },
+      light: function () {
+        return Font.lightSystemFont(fontSize);
+      },
+      regular: function () {
+        return Font.regularSystemFont(fontSize);
+      },
+      medium: function () {
+        return Font.mediumSystemFont(fontSize);
+      },
+      semibold: function () {
+        return Font.semiboldSystemFont(fontSize);
+      },
+      bold: function () {
+        return Font.boldSystemFont(fontSize);
+      },
+      heavy: function () {
+        return Font.heavySystemFont(fontSize);
+      },
+      black: function () {
+        return Font.blackSystemFont(fontSize);
+      },
+      italic: function () {
+        return Font.italicSystemFont(fontSize);
+      },
+    };
+
+    const systemFont = fontGenerator[fontName];
+    if (systemFont) {
+      return systemFont();
+    }
+    return new Font(fontName, fontSize);
+  }
+
+  setWidget = async (widget) => {
+    return widget;
+  };
+
+  renderErrorWidget = (widget, msg = "") => {
+    widget.addText(JSON.stringify(msg) || "暂不支持该尺寸组件");
+    return widget;
+  };
+
+  renderSmall = async (widget) => {
+    return await this.setWidget(widget);
+  };
+
+  renderMedium = async (widget) => {
+    await this.setWidget(widget);
+    return widget;
+  };
+
+  renderLarge = async (widget) => {
+    await this.setWidget(widget);
+    return widget;
+  };
+
+  renderBefor = async (w) => {
+    return w;
+  };
+
+  render = async () => {
+    const widget = new ListWidget();
+    widget.setPadding(10, 10, 10, 10);
+    let w = await this.renderBefor(widget);
+
+    switch (this.widgetSize) {
+      case "small": {
+        w = await this.renderSmall(w);
+        w.presentSmall();
+        break;
+      }
+      case "medium": {
+        w = await this.renderMedium(w);
+        w.presentMedium();
+        break;
+      }
+      case "large": {
+        w = await this.renderLarge(w);
+        w.presentLarge();
+        break;
+      }
+      default: {
+        w = await this.renderErrorWidget(w);
+        w.presentSmall();
+        break;
+      }
+    }
+
+    Script.setWidget(w);
+    Script.complete();
+  };
+}
+module.exports = { DmYY };
---
+++ JD/index.js
@@ -0,0 +1,226 @@
+/*
+ * Author: 2Ya
+ * Github: https://github.com/dompling
+ * 本脚本使用了@Gideon_Senku的Env
+ */
+// const { DmYY } = require("../DmYY");
+const { DmYY } = importModule("DmYY");
+
+const blurBackground = true; // 开启背景虚化 true 值类型布尔或数字 ，默认 0.7 取值范围 0 至 1
+const imageBackground = true; // 设置配置背景图片
+const forceImageUpdate = false; // 设置为true将重置小部件的背景图像
+
+const textFont = {
+  title: { size: 22, color: "FFF", font: "semibold" },
+  desc: { size: 14, color: "fff", font: "black" },
+};
+
+class YaYaJD extends DmYY {
+  constructor(widgetParameter) {
+    super();
+    this.jdIndex = parseInt(widgetParameter) || 0;
+  }
+
+  forceImageUpdate = forceImageUpdate;
+  blurBackground = blurBackground;
+  imageBackground = imageBackground;
+  CookiesJD = [];
+  cookie = "";
+  widgetSize = "medium";
+
+  userName = "";
+  beanCount = 0;
+  incomeBean = 0;
+  expenseBean = 0;
+  errorMsg = "";
+
+  init = async () => {
+    try {
+      this.CookiesJD = await this.getCache("CookiesJD");
+      this.cookie = this.CookiesJD[this.jdIndex].cookie;
+      this.userName = this.CookiesJD[this.jdIndex].userName;
+      await this.TotalBean();
+      await this.bean();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  bean = async () => {
+    //前一天的0:0:0时间戳
+    // console.log(`北京时间零点时间戳:${parseInt((Date.now() + 28800000) / 86400000) * 86400000 - 28800000}`);
+    // console.log(`北京时间2020-10-28 06:16:05::${new Date("2020/10/28 06:16:05+08:00").getTime()}`)
+    const tm =
+      parseInt((Date.now() + 28800000) / 86400000) * 86400000 -
+      28800000 -
+      24 * 60 * 60 * 1000;
+    // 今天0:0:0时间戳
+    const tm1 =
+      parseInt((Date.now() + 28800000) / 86400000) * 86400000 - 28800000;
+    let page = 1,
+      t = 0;
+    do {
+      let response = await this.getJingBeanBalanceDetail(page);
+      console.log(`第${page}页`);
+      if (response && response.code === "0") {
+        page++;
+        let detailList = response.detailList;
+        if (detailList && detailList.length > 0) {
+          for (let item of detailList) {
+            const date = item.date.replace(/-/g, "/") + "+08:00";
+            if (
+              tm <= new Date(date).getTime() &&
+              new Date(date).getTime() < tm1
+            ) {
+              //昨日的
+              if (Number(item.amount) > 0) {
+                this.incomeBean += Number(item.amount);
+              } else if (Number(item.amount) < 0) {
+                this.expenseBean += Number(item.amount);
+              }
+            } else if (tm > new Date(date).getTime()) {
+              //前天的
+              t = 1;
+              break;
+            }
+          }
+        } else {
+          console.log(`账号${this.jdIndex}：${this.userName}\n数据异常`);
+          t = 1;
+        }
+      }
+    } while (t === 0);
+    // console.log(`昨日收入：${$.incomeBean}个京豆 🐶`);
+    // console.log(`昨日支出：${$.expenseBean}个京豆 🐶`)
+  };
+
+  TotalBean = async () => {
+    const options = {
+      headers: {
+        Accept: "application/json,text/plain, */*",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-cn",
+        Connection: "keep-alive",
+        Cookie: this.cookie,
+        Referer: "https://wqs.jd.com/my/jingdou/my.shtml?sceneval=2",
+        "User-Agent":
+          "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+      },
+    };
+    const url = "https://wq.jd.com/user/info/QueryJDUserInfo?sceneval=2";
+    const response = await this.$request.post(url, options, "JSON");
+    if (response.retcode === 0) {
+      this.beanCount = response.base.jdNum;
+    } else {
+      console.log("京东服务器返回空数据");
+    }
+    return response;
+  };
+
+  getJingBeanBalanceDetail = async (page) => {
+    const url =
+      "https://api.m.jd.com/client.action?functionId=getJingBeanBalanceDetail";
+    const options = {
+      body: `body=${escape(
+        JSON.stringify({ pageSize: "20", page: page.toString() })
+      )}&appid=ld`,
+      headers: {
+        "User-Agent": "JD4iPhone/167169 (iPhone; iOS 13.4.1; Scale/3.00)",
+        Host: "api.m.jd.com",
+        "Content-Type": "application/x-www-form-urlencoded",
+        Cookie: this.cookie,
+      },
+    };
+    return await this.$request.post(url, options);
+  };
+
+  setContainer = async (container, { icon, text, desc }) => {
+    container.layoutVertically();
+    container.centerAlignContent();
+    container.size = new Size(100, 120);
+    const viewer = container.addStack();
+    viewer.size = new Size(100, 30);
+    const jdD_icon = await this.$request.get(icon, "IMG");
+    const imageElemView = viewer.addImage(jdD_icon);
+    imageElemView.centerAlignImage();
+    imageElemView.imageSize = new Size(30, 30);
+    container.addSpacer(10);
+
+    const textview = container.addStack();
+    textview.centerAlignContent();
+    textview.size = new Size(100, 40);
+    const titleTextItem = this.setCellText(
+      text,
+      textview,
+      textFont.title,
+      false
+    );
+    titleTextItem.rightAlignText();
+
+    const descView = container.addStack();
+    descView.centerAlignContent();
+    descView.size = new Size(100, 30);
+    const descTextItem = this.setCellText(desc, descView, textFont.desc);
+    descTextItem.centerAlignText();
+
+    return container;
+  };
+
+  setWidget = async (widget) => {
+    const body = widget.addStack();
+    body.centerAlignContent();
+    body.url = "https://bean.m.jd.com/";
+    const letfContainer = body.addStack();
+    await this.setContainer(letfContainer, {
+      icon:
+        "https://raw.githubusercontent.com/dompling/Scriptable/master/JD/jdd.png",
+      text: `${this.beanCount}`,
+      desc: "当前京豆",
+    });
+    body.addSpacer(10);
+    const centerContainer = body.addStack();
+    await this.setContainer(centerContainer, {
+      icon:
+        "https://raw.githubusercontent.com/dompling/Scriptable/master/JD/jdd.png",
+      text: `+${this.incomeBean}`,
+      desc: "昨日收入",
+    });
+    body.addSpacer(10);
+    const rightContainer = body.addStack();
+    await this.setContainer(rightContainer, {
+      icon:
+        "https://raw.githubusercontent.com/dompling/Scriptable/master/JD/jdd.png",
+      text: `-${this.incomeBean}`,
+      desc: "昨日支出",
+    });
+    return widget;
+  };
+
+  renderBefor = async (w) => {
+    const icon = "https://raw.githubusercontent.com/Orz-3/task/master/jd.png";
+    await this.setHeader(w, { title: "京豆数量", icon }, (body) => {
+      body.addSpacer(140);
+      const headerRight = body.addStack();
+      // headerRight.size = new Size(30, 20);
+      headerRight.setPadding(1, 10, 1, 10);
+      headerRight.cornerRadius = 10;
+      headerRight.backgroundColor = new Color("#fff", 0.5);
+      const textItem = this.provideText(this.userName, headerRight);
+      textItem.centerAlignText();
+      return body;
+    });
+
+    w.addSpacer(15);
+    await this.setWidgetBackGround(w);
+    return w;
+  };
+
+  renderLarge = async (w) => {
+    return this.renderErrorWidget(w);
+  };
+}
+
+const _2YaJD = new YaYaJD(args.widgetParameter);
+await _2YaJD.init(); // 初始化数据
+await _2YaJD.render(); // 加载widget
