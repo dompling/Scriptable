4fe34a89a2e61297583fe927665bfdd19bd2ba67
dompling <374779789@qq.com>
2020-11-05 20:10:50 +0800

    提交

--- widget.Install.js
+++ widget.Install.js
@@ -2,156 +2,131 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: teal; icon-glyph: book-open;
 // Version 1.0.0
-const defaultBaseURL = "https://widget-hub.app";
-const catalogURL = "/api/list";
-const catalogPageURL = "/catalog";
-const webView = new WebView();
-let localVersions = [];
 
-// Setup Filemanager and paths
-const fmCloud = FileManager.iCloud();
+const mainAlert = new Alert();
+const Files = FileManager.iCloud();
+const RootPath = Files.documentsDirectory();
 
-async function loadScript(url) {
+const saveFile = async ({ moduleName, url }) => {
+  const header = `// Variables used by Scriptable.
+  // These must be at the very top of the file. Do not edit.
+  // icon-color: deep-gray; icon-glyph: file-code;\n`;
   const req = new Request(url);
   const content = await req.loadString();
-  const filename = url.split("/").pop();
+  const fileHeader = content.includes("icon-color") ? `` : header;
+  write(`${moduleName}`, `${fileHeader}${content}`);
+};
 
-  return {
-    content,
-    filename,
-  };
-}
+const saveFileName = (fileName) => {
+  const hasSuffix = fileName.lastIndexOf(".") + 1;
+  return !hasSuffix ? `${fileName}.js` : fileName;
+};
 
-const downloadWidget = async function (widget) {
-  const downloadAlert = new Alert();
-  downloadAlert.message = `Do you like to Download the  '${widget.name}' Widget-Script?`;
-  downloadAlert.addAction("Yes");
-  downloadAlert.addCancelAction("No");
+const write = (fileName, content) => {
+  let file = saveFileName(fileName);
+  const filePath = `${RootPath}/${file}`;
+  Files.writeString(filePath, content);
+  return true;
+};
 
-  if ((await downloadAlert.presentAlert()) === 0) {
-    const { content, filename } = await loadScript(widget.scriptURL);
+const notify = async (title, body, url, opts = {}) => {
+  let n = new Notification();
+  n = Object.assign(n, opts);
+  n.title = title;
+  n.body = body;
+  if (url) n.openURL = url;
+  return await n.schedule();
+};
 
-    const scriptPath = fmCloud.joinPath(fmCloud.documentsDirectory(), filename);
-    const scriptExists = fmCloud.fileExists(scriptPath);
-
-    if (scriptExists) {
-      const alreadyExistsAlert = new Alert();
-      alreadyExistsAlert.message = `The Script '${filename}' already exists!`;
-      alreadyExistsAlert.addAction("Replace");
-      alreadyExistsAlert.addCancelAction("Cancel");
-
-      if ((await alreadyExistsAlert.presentAlert()) === -1) {
-        return false;
-      }
-    }
-    fmCloud.writeString(scriptPath, content);
-
-    const successAlert = new Alert();
-    if (widget.configurationRequired === true) {
-      successAlert.message = `Widget-Script '${filename}' saved. The widget requires manual configuration. Do you want to open the homepage of the widget for further instructions?`;
-      successAlert.addAction("Yes, show Widget Homepage");
-      successAlert.addCancelAction("No");
-    } else {
-      successAlert.message = `Widget-Script '${filename}' saved and ready to use!`;
-      successAlert.addCancelAction("Close");
-    }
-    if ((await successAlert.presentAlert()) === 0) {
-      Safari.openInApp(widget.homepage);
-    }
-    return filename;
+const renderTableList = async (data) => {
+  try {
+    const table = new UITable();
+    // 如果是节点，则先远程获取
+    const req = new Request(data.subscription);
+    const subscription = await req.loadJSON();
+    const apps = subscription.apps;
+    apps.forEach((item) => {
+      const r = new UITableRow();
+      const imgCell = new UITableCell();
+      imgCell.centerAligned();
+      imgCell.imageAtURL(item.thumb);
+      r.addCell(imgCell);
+      const nameCell = new UITableCell();
+      nameCell.centerAligned();
+      nameCell.text(item.name);
+      r.addCell(nameCell);
+      const downloadCell = new UITableCell();
+      downloadCell.centerAligned();
+      downloadCell.button("下载");
+      downloadCell.onTap(async () => {
+        const isWrite = await write(item.scriptURL);
+        if (isWrite) {
+          notify("下载提示", `插件:${item.name}.js 下载/更新成功`);
+        }
+      });
+      r.addCell(downloadCell);
+      table.addRow(r);
+    });
+    table.present(false);
+  } catch (e) {
+    console.log(e);
+    notify("错误提示", "订阅获取失败");
   }
 };
 
-const updateWidget = async function (widget) {
-  const downloadAlert = new Alert();
-  downloadAlert.message = `Do you like to Update the  '${widget.name}' Widget-Script?`;
-  downloadAlert.addAction("Yes");
-  downloadAlert.addCancelAction("No");
-
-  if ((await downloadAlert.presentAlert()) === 0) {
-    const { content, filename } = await loadScript(widget.scriptURL);
-
-    const scriptPath = fmCloud.joinPath(fmCloud.documentsDirectory(), filename);
-    fmCloud.writeString(scriptPath, content);
-
-    await injectLocalVersions();
-
-    const successAlert = new Alert();
-    if (widget.configurationRequired === true) {
-      successAlert.message = `Widget-Script '${filename}' updated. The widget requires manual configuration. Do you want to open the homepage of the widget for further instructions?`;
-      successAlert.addAction("Yes, show Widget Homepage");
-      successAlert.addCancelAction("No");
-    } else {
-      successAlert.message = `Widget-Script '${filename}' updated and ready to use!`;
-      successAlert.addCancelAction("Close");
-    }
-    if ((await successAlert.presentAlert()) === 0) {
-      Safari.openInApp(widget.homepage);
-    }
-    return filename;
+mainAlert.title = "组件下载";
+mainAlert.message = "可以自行添加订阅地址";
+try {
+  const cacheKey = "subscriptionList";
+  const subscriptionList = [];
+  if (Keychain.contains(cacheKey)) {
+    subscriptionList = Keychain.get(cacheKey);
   }
-};
-
-async function fetchCatalog(url) {
-  const req = new Request(url);
-  return await req.loadJSON();
-}
-
-async function injectLocalVersions() {
-  const js =
-    `window.localWidgets = JSON.parse('` +
-    JSON.stringify(localVersions) +
-    `'); window.dispatchEvent(new CustomEvent('local-widget-versions-added'));`;
-  return webView.evaluateJavaScript(js);
-}
-
-async function injectEventhandler() {
-  const js = `
-    window.addEventListener('catalog-event', (event) => {  
-      completion(event.detail)
-    }, false);
-  `;
-  return webView.evaluateJavaScript(js, true).then(async (event) => {
-    if (event.name === "downloadButtonClicked") {
-      await downloadWidget(event.widget);
-    } else if (event.name === "updateButtonClicked") {
-      await updateWidget(event.widget);
-    } else if (event.name === "linkClicked") {
-      Safari.openInApp(event.url);
-    }
-    return injectEventhandler();
+  const _actions = [];
+  subscriptionList.forEach((item) => {
+    const { author } = item;
+    mainAlert.addAction("作者：" + author);
+    _actions.push(async () => {
+      await this.renderTableList(item);
+    });
   });
-}
 
-present = async (b) => {
-  const baseUrl = !b ? defaultBaseURL : b;
-  const catalog = await fetchCatalog(baseUrl + catalogURL);
-
-  // Check Version of local Script
-  localVersions = catalog.map((w) => {
-    const versions = {
-      localVersion: "",
-      name: w.name,
-    };
-    const filename = w.scriptURL.split("/").pop();
-    const scriptPath = fmCloud.joinPath(fmCloud.documentsDirectory(), filename);
-    const scriptExists = fmCloud.fileExists(scriptPath);
-
-    if (scriptExists) {
-      const scriptContent = fmCloud.readString(scriptPath);
-      const m = scriptContent.match(/Version[\s]*([\d]+(\.[\d]+){0,2})/m);
-      if (m && m[1]) {
-        versions.localVersion = m[1];
+  _actions.push(async () => {
+    const a = new Alert();
+    a.title = "订阅地址";
+    a.addTextField("URL");
+    a.addAction("确定");
+    a.addCancelAction("取消");
+    const id = await a.presentAlert();
+    if (id === -1) return;
+    try {
+      const url = a.textFieldValue(0);
+      const response = await new Request().loadJSON();
+      const data = [];
+      for (let _ in subscriptionList) {
+        if (response.author === subscriptionList[i].author) {
+          data.push({ ...response, subscription: url });
+        } else {
+          data.push(subscriptionList[i]);
+        }
       }
+      if (!subscriptionList.length)
+        data.push({ ...response, subscription: url });
+      Keychain.set(cacheKey, JSON.stringify(response));
+    } catch (e) {
+      console.log(e);
+      notify("错误提示", "订阅地址错误，不是一个 JSON 格式");
     }
-
-    return versions;
   });
-  const querParams = Device.isUsingDarkAppearance() ? "?darkMode" : "";
-  await webView.loadURL(baseUrl + catalogPageURL + querParams);
-  await injectLocalVersions();
-  injectEventhandler();
-  return webView.present();
-};
 
-present();
+  mainAlert.addAction("添加订阅");
+
+  mainAlert.addCancelAction("取消操作");
+  const _actionsIndex = await mainAlert.presentSheet();
+  if (_actions[_actionsIndex]) {
+    const func = _actions[_actionsIndex];
+    await func();
+  }
+} catch (e) {
+  console.log("缓存读取错误" + e);
+}
