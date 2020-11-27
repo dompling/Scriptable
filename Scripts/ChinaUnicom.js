33535150568e28e392aa221cd026135a0bff5aeb
2YA <374779789@qq.com>
2020-11-27 19:11:26 +0800

    Update ChinaUnicom.js

--- Scripts/ChinaUnicom.js
+++ Scripts/ChinaUnicom.js
@@ -321,14 +321,14 @@ class Widget extends DmYY {
       this.registerAction('基础设置', this.setWidgetConfig);
     }
     const {loginheader, step1, step2, step3, step4, inner, icon, percent, value} = this.settings;
-    this.fgCircleColor = new Color(inner) || this.fgCircleColor;
-    this.textColor1 = new Color( value) || this.textColor1;
-    this.circleColor1 = new Color( step1) || this.circleColor1;
-    this.circleColor2 = new Color( step2) || this.circleColor2;
-    this.circleColor3 = new Color( step3) || this.circleColor3;
-    this.circleColor4 = new Color( step4) || this.circleColor4;
-    this.iconColor = new Color( icon) || this.iconColor;
-    this.percentColor = new Color( percent) || this.percentColor;
+    this.fgCircleColor = inner? new Color(inner) : this.fgCircleColor;
+    this.textColor1 = value? new Color( value) : this.textColor1;
+    this.circleColor1 = step1? new Color( step1) : this.circleColor1;
+    this.circleColor2 = step2?new Color( step2) : this.circleColor2;
+    this.circleColor3 = step3?new Color( step3) : this.circleColor3;
+    this.circleColor4 = step4? new Color( step4) : this.circleColor4;
+    this.iconColor = icon? new Color( icon) :this.iconColor;
+    this.percentColor = percent? new Color( percent) : this.percentColor;
 
     
     this.loginheader = loginheader ? JSON.parse(loginheader) : {};
