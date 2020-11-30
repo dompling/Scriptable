fc062273f7945da20a7210d4512f9cb6d6ab188c
dompling <374779789@qq.com>
2020-11-30 21:38:56 +0800

    tip:增加机场面板显示，测试中

---
+++ Scripts/VpnBoard.js
@@ -0,0 +1,452 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-gray; icon-glyph: paper-plane;
+
+// 添加require，是为了vscode中可以正确引入包，以获得自动补全等功能
+if (typeof require === 'undefined') require = importModule;
+const {DmYY, Runing} = require('./DmYY');
+
+// @组件代码开始
+class Widget extends DmYY {
+  constructor(arg) {
+    super(arg);
+    this.name = 'Vpn';
+    this.en = 'vpnBoard';
+    this.CACHE_KEY = this.md5(`dataSouce_${this.en}`);
+    this.Run();
+  }
+
+  useBoxJS = false;
+  today = '';
+  loginok = false;
+  dataSource = {
+    restData: '0',
+    usedData: '0',
+    todayUsed: '0',
+    isCheckIn: false,
+  };
+
+  chartConfig = (labels = [], datas = []) => {
+    const color = `#${this.widgetColor.hex}`;
+    return `{
+    'type': 'bar', // line 类型为折现类型
+    'data': {
+      'labels': ${JSON.stringify(labels)},
+      'datasets': [
+        {
+          type: 'line',
+          backgroundColor: '#fff',
+          borderColor: getGradientFillHelper('vertical', ['#c8e3fa', '#e62490']),
+          'borderWidth': 2,
+          pointRadius: 5,
+          'fill': false,
+          'data': ${JSON.stringify(datas)},
+        },
+      ],
+    },
+    'options': {
+      plugins: {
+        datalabels: {
+          display: true,
+          align: 'top',
+          color: '${color}',
+        },
+      },
+      layout: {
+          padding: {
+              left: 0,
+              right: 0,
+              top: 10,
+              bottom: 0
+          }
+      },
+      responsive: true,
+      maintainAspectRatio: true,
+      'legend': {
+        'display': false,
+      },
+      'title': {
+        'display': false,
+      },
+      scales: {
+        xAxes: [ // X 轴线
+          {
+            gridLines: {
+              display: false,
+              color: '${color}',
+            },
+            ticks: {
+              display: true, 
+              fontColor: '${color}',
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              display: false,
+              beginAtZero: false,
+              fontColor: '${color}',
+            },
+            gridLines: {
+              borderDash: [7, 5],
+              display: false,
+              color: '${color}',
+            },
+          },
+        ],
+      },
+    },
+  }`;
+  };
+
+  account = {
+    title: '',
+    url: '',
+    email: '',
+    password: '',
+  };
+
+  range = {};
+  max = 6;
+
+  init = async () => {
+    try {
+      await this.login();
+      await this.checkin();
+      await this.dataResults();
+      if (Keychain.contains(this.CACHE_KEY)) {
+        this.range = JSON.parse(Keychain.get(this.CACHE_KEY));
+      }
+      const date = new Date();
+      const format = new DateFormatter();
+      format.dateFormat = 'MM.dd';
+      const dateDay = format.string(date);
+      this.range[dateDay] = this.dataSource;
+      const rangeKey = Object.keys(this.range);
+      if (rangeKey.length > this.max) {
+        for (let i = 0; i <= rangeKey.length - this.max; i++) {
+          delete this.range[rangeKey[i]];
+        }
+      }
+      Keychain.set(this.CACHE_KEY, JSON.stringify(this.range));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  async login() {
+    const url = this.account.url;
+    const loginPath =
+        url.indexOf('auth/login') != -1 ? 'auth/login' : 'user/_login.php';
+    const table = {
+      url:
+          url.replace(/(auth|user)\/login(.php)*/g, '') +
+          loginPath +
+          `?email=${this.account.email}&passwd=${this.account.password}&rumber-me=week`,
+    };
+
+    const data = await this.$request.post(table, 'STRING');
+    try {
+      if (
+          JSON.parse(data).msg.match(
+              /邮箱不存在|邮箱或者密码错误|Mail or password is incorrect/,
+          )
+      ) {
+        this.loginok = false;
+        this.notify(this.name, '邮箱或者密码错误');
+        console.log('登陆失败');
+      } else {
+        this.loginok = true;
+        console.log('登陆成功');
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async checkin() {
+    const url = this.account.url;
+    let checkinPath =
+        url.indexOf('auth/login') != -1 ? 'user/checkin' : 'user/_checkin.php';
+    const checkinreqest = {
+      url: url.replace(/(auth|user)\/login(.php)*/g, '') + checkinPath,
+    };
+    const data = await this.$request.post(checkinreqest, 'STRING');
+    if (data.match(/\"msg\"\:/)) {
+      console.log('签到成功');
+      this.dataSource.isCheckIn = true;
+    } else {
+      console.log('签到失败');
+    }
+  }
+
+  async dataResults() {
+    const url = this.account.url;
+    const userPath = url.indexOf('auth/login') != -1
+        ? 'user'
+        : 'user/index.php';
+    const datarequest = {
+      url: url.replace(/(auth|user)\/login(.php)*/g, '') + userPath,
+    };
+    const data = await this.$request.get(datarequest, 'STRING');
+    if (data.match(/login|请填写邮箱|登陆/)) {
+      this.loginok = false;
+    } else {
+      let resultData = '';
+      let result = [];
+      if (data.match(/theme\/malio/)) {
+        let flowInfo = data.match(/trafficDountChat\s*\(([^\)]+)/);
+        if (flowInfo) {
+          let flowData = flowInfo[1].match(/\d[^\']+/g);
+          let usedData = flowData[0];
+          let todatUsed = flowData[1];
+          let restData = flowData[2];
+          this.dataSource.todayUsed = `${flowData[1]}`;
+          this.dataSource.usedData = `${flowData[0]}`;
+          this.dataSource.restData = `${flowData[2]}`;
+          result.push(
+              `今日：${todatUsed}\n已用：${usedData}\n剩余：${restData}`,
+          );
+        }
+        let userInfo = data.match(/ChatraIntegration\s*=\s*({[^}]+)/);
+        if (userInfo) {
+          let user_name = userInfo[1].match(/name.+'(.+)'/)[1];
+          let user_class = userInfo[1].match(/Class.+'(.+)'/)[1];
+          let class_expire = userInfo[1].match(/Class_Expire.+'(.+)'/)[1];
+          let money = userInfo[1].match(/Money.+'(.+)'/)[1];
+          result.push(
+              `用户名：${user_name}\n用户等级：lv${user_class}\n余额：${money}\n到期时间：${class_expire}`,
+          );
+        }
+        if (result.length != 0) {
+          resultData = result.join('\n\n');
+        }
+      } else {
+        let todayUsed = data.match(/>*\s*今日(已用|使用)*[^B]+/);
+        if (todayUsed) {
+          todayUsed = this.flowFormat(todayUsed[0]);
+          result.push(`今日：${todayUsed}`);
+          this.dataSource.todayUsed = `${todayUsed}`;
+        } else {
+          this.dataSource.todayUsed = `0`;
+          result.push(`今日已用获取失败`);
+        }
+        let usedData = data.match(
+            /(Used Transfer|>过去已用|>已用|>总已用|\"已用)[^B]+/,
+        );
+        if (usedData) {
+          usedData = this.flowFormat(usedData[0]);
+          result.push(`已用：${usedData}`);
+          this.dataSource.usedData = `${usedData}`;
+        } else {
+          this.dataSource.usedData = `0`;
+          result.push(`累计使用获取失败`);
+        }
+        let restData = data.match(
+            /(Remaining Transfer|>剩余流量|>流量剩余|>可用|\"剩余)[^B]+/,
+        );
+        if (restData) {
+          restData = this.flowFormat(restData[0]);
+          result.push(`剩余：${restData}`);
+          this.dataSource.restData = `${restData}`;
+        } else {
+          this.dataSource.restData = `0`;
+          result.push(`剩余流量获取失败`);
+        }
+        resultData = result.join('\n');
+      }
+      console.log(resultData);
+    }
+  }
+
+  flowFormat(data) {
+    data = data.replace(/\d+(\.\d+)*%/, '');
+    let flow = data.match(/\d+(\.\d+)*\w*/);
+    return flow[0] + 'B';
+  }
+
+  createChart = async () => {
+    const labels = [], data = [];
+    Object.keys(this.range).forEach(key => {
+      labels.push(key);
+      data.push(this.range[key].todayUsed);
+    });
+    const chart = this.chartConfig(labels, data);
+    const url = `https://quickchart.io/chart?w=390&h=85&f=png&c=${encodeURIComponent(
+        chart)}`;
+    return await this.$request.get(url, 'IMG');
+  };
+
+  createDivider(w) {
+    const drawContext = new DrawContext();
+    drawContext.size = new Size(543, 2);
+    const path = new Path();
+    path.addLine(new Point(1000, 20));
+    drawContext.addPath(path);
+    drawContext.setStrokeColor(new Color('#000', 1));
+    drawContext.setLineWidth(2);
+    drawContext.strokePath();
+
+    const stackLine = w.addStack();
+    stackLine.borderWidth = 1;
+    stackLine.borderColor = new Color('#000', 0.2);
+    stackLine.addImage(drawContext.getImage());
+    w.addSpacer(5);
+  }
+
+  setHeader(w, size) {
+    const header = w.addStack();
+    header.addSpacer();
+    const left = header.addStack();
+    const vpnName = {...this.textFormat.title};
+    vpnName.size = size;
+    this.provideText(this.account.title, left, vpnName);
+
+    header.addSpacer(100);
+
+    const right = header.addStack();
+    right.bottomAlignContent();
+    const vpnFlow = {...this.textFormat.title};
+    vpnFlow.color = new Color('#26c5bc');
+    vpnFlow.size = size;
+    this.provideText(this.dataSource.isCheckIn ? '已签到' : '未签到', right, vpnFlow);
+    header.addSpacer();
+
+    w.addSpacer();
+  };
+
+  setLabel(w, data) {
+    const stackLabel = w.addStack();
+    const textBattery = {...this.textFormat.battery};
+    textBattery.size = 14;
+    this.provideText(data.label, stackLabel, textBattery);
+
+    textBattery.size = 20;
+    const stackValue = w.addStack();
+    stackValue.centerAlignContent();
+    textBattery.color = new Color(data.color);
+    this.provideText(data.value, stackValue, textBattery);
+  }
+
+  setFooter(w) {
+    const footer = w.addStack();
+    footer.centerAlignContent();
+
+    this.setLabel(
+        footer, {
+          label: '剩余：',
+          color: '#ff3e3e',
+          value: this.dataSource.restData,
+          unit: 'GB',
+        });
+    footer.addSpacer();
+    this.setLabel(
+        footer, {
+          label: '累计：',
+          color: '#dc9e28',
+          value: this.dataSource.usedData,
+          unit: 'GB',
+        });
+  }
+
+  setContent = async (w) => {
+    const imageFlow = await this.createChart();
+    const stackContent = w.addStack();
+    stackContent.addImage(imageFlow);
+  };
+
+  renderSmall = async (w) => {
+    this.setHeader(w, 14);
+    await this.setContent(w);
+    this.createDivider(w);
+    this.setFooter(w, 14);
+    return w;
+  };
+
+  renderMedium = async (w) => {
+    this.setHeader(w, 16);
+    await this.setContent(w);
+    this.createDivider(w);
+    this.setFooter(w);
+    return w;
+  };
+
+  renderLarge = async (w) => {
+    w.addText('暂不支持');
+    return w;
+  };
+
+  /**
+   * 渲染函数，函数名固定
+   * 可以根据 this.widgetFamily 来判断小组件尺寸，以返回不同大小的内容
+   */
+  async render() {
+    await this.init();
+    const widget = new ListWidget();
+    await this.getWidgetBackgroundImage(widget);
+    if (this.widgetFamily === 'medium') {
+      return await this.renderMedium(widget);
+    } else if (this.widgetFamily === 'large') {
+      return await this.renderLarge(widget);
+    } else {
+      return await this.renderSmall(widget);
+    }
+  }
+
+  Run = () => {
+    if (config.runsInApp) {
+      this.registerAction('默认账号', this.actionSettings);
+      this.registerAction('新增账号', async () => {
+        const account = await this.setAlertInput(
+            '添加账号', '添加账号数据，添加完成之后请去设置默认账号', {
+              title: '机场名',
+              url: '登陆地址',
+              email: '邮箱账号',
+              password: '密码',
+            }, false);
+        if (!this.settings.dataSource) this.settings.dataSource = [];
+        if (!account) return;
+        if (account.title && account.url && account.email && account.password) {
+          this.settings.dataSource.push(account);
+        }
+        this.settings.dataSource = this.settings.dataSource.filter(
+            item => item);
+        this.saveSettings();
+      });
+      this.registerAction('基础设置', this.setWidgetConfig);
+    }
+    this.account = this.settings.account || this.account;
+    this.CACHE_KEY += '_' + this.account.title;
+    const index = typeof args.widgetParameter === 'string' ? parseInt(
+        args.widgetParameter) : false;
+    if (this.settings[index] && index !== false) {
+      this.account = this.settings[index];
+    }
+  };
+
+  async actionSettings() {
+    try {
+      const table = new UITable();
+      const dataSource = this.settings.dataSource || [];
+      dataSource.map((t) => {
+        const r = new UITableRow();
+        r.addText(`机场名：${t.title}     账号：${t.email}`);
+        r.onSelect = (n) => {
+          this.settings.account = t;
+          this.notify(t.title, `默认账号设置成功\n账号：${t.email}`);
+          this.saveSettings(false);
+        };
+        table.addRow(r);
+      });
+      table.present(false);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+}
+
+// @组件代码结束
+// await Runing(Widget, "", false); // 正式环境
+await Runing(Widget, '', false); //远程开发环境
