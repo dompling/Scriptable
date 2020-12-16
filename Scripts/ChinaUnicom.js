222ebc3cd4448a6fce78e46cbffbb3b595de3475
dompling <374779789@qq.com>
2020-12-16 13:40:02 +0800

    修复联通更新时间

--- Scripts/ChinaUnicom.js
+++ Scripts/ChinaUnicom.js
@@ -288,7 +288,7 @@ class Widget extends DmYY {
     const text = this.textFormat.defaultText;
     text.color = new Color("#aaa");
     this.provideText(
-      `电信更新：${this.arrUpdateTime[2]}:${this.arrUpdateTime[3]}`,
+      `联通更新：${this.arrUpdateTime[2]}:${this.arrUpdateTime[3]}`,
       stackFooter,
       text
     );
