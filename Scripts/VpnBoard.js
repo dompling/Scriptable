26790b4836bda12632fc8cea0de35dc95518d895
dompling <374779789@qq.com>
2020-12-23 14:21:25 +0800

    update:优化机场显示数据

--- Scripts/VpnBoard.js
+++ Scripts/VpnBoard.js
@@ -17,8 +17,6 @@ class Widget extends DmYY {
   }
 
   useBoxJS = false;
-  today = '';
-  loginok = false;
   dataSource = {
     restData: '0',
     usedData: '0',
@@ -172,28 +170,25 @@ module.exports = template;`;
   };
 
   async login() {
-    const url = this.account.url;
-    const loginPath =
-        url.indexOf('auth/login') != -1 ? 'auth/login' : 'user/_login.php';
     const table = {
-      url:
-          url.replace(/(auth|user)\/login(.php)*/g, '') +
-          loginPath +
-          `?email=${this.account.email}&passwd=${this.account.password}&rumber-me=week`,
+      url: this.account.url,
+      body: `email=${encodeURIComponent(
+          this.account.email)}&passwd=${encodeURIComponent(
+          this.account.password)}&remember_me=on&rumber-me=week`,
     };
-
-    const data = await this.$request.post(table, 'STRING');
+    const request = new Request(table.url);
+    request.body = table.body;
+    request.method = 'POST';
+    const data = await request.loadString();
     try {
       if (
           JSON.parse(data).msg.match(
               /邮箱不存在|邮箱或者密码错误|Mail or password is incorrect/,
           )
       ) {
-        this.loginok = false;
         this.notify(this.name, '邮箱或者密码错误');
         console.log('登陆失败');
       } else {
-        this.loginok = true;
         console.log('登陆成功');
       }
     } catch (e) {
@@ -204,7 +199,7 @@ module.exports = template;`;
   async checkin() {
     const url = this.account.url;
     let checkinPath =
-        url.indexOf('auth/login') != -1 ? 'user/checkin' : 'user/_checkin.php';
+        url.indexOf('auth/login') !== -1 ? 'user/checkin' : 'user/_checkin.php';
     const checkinreqest = {
       url: url.replace(/(auth|user)\/login(.php)*/g, '') + checkinPath,
     };
@@ -219,87 +214,23 @@ module.exports = template;`;
 
   async dataResults() {
     const url = this.account.url;
-    const userPath = url.indexOf('auth/login') != -1
-        ? 'user'
-        : 'user/index.php';
-    const datarequest = {
-      url: url.replace(/(auth|user)\/login(.php)*/g, '') + userPath,
-    };
-    const data = await this.$request.get(datarequest, 'STRING');
-    if (data.match(/login|请填写邮箱|登陆/)) {
-      this.loginok = false;
-    } else {
-      let resultData = '';
-      let result = [];
-      if (data.match(/theme\/malio/)) {
-        let flowInfo = data.match(/trafficDountChat\s*\(([^\)]+)/);
-        if (flowInfo) {
-          let flowData = flowInfo[1].match(/\d[^\']+/g);
-          let usedData = flowData[0];
-          let todatUsed = flowData[1];
-          let restData = flowData[2];
-          this.dataSource.todayUsed = todatUsed;
-          this.dataSource.usedData = usedData;
-          this.dataSource.restData = restData;
-          result.push(
-              `今日：${todatUsed}\n已用：${usedData}\n剩余：${restData}`,
-          );
-        }
-        let userInfo = data.match(/ChatraIntegration\s*=\s*({[^}]+)/);
-        if (userInfo) {
-          let user_name = userInfo[1].match(/name.+'(.+)'/)[1];
-          let user_class = userInfo[1].match(/Class.+'(.+)'/)[1];
-          let class_expire = userInfo[1].match(/Class_Expire.+'(.+)'/)[1];
-          let money = userInfo[1].match(/Money.+'(.+)'/)[1];
-          result.push(
-              `用户名：${user_name}\n用户等级：lv${user_class}\n余额：${money}\n到期时间：${class_expire}`,
-          );
-        }
-        if (result.length != 0) {
-          resultData = result.join('\n\n');
-        }
-      } else {
-        let todayUsed = data.match(/>*\s*今日(已用|使用)*[^B]+/);
-        if (todayUsed) {
-          todayUsed = this.flowFormat(todayUsed[0]);
-          result.push(`今日：${todayUsed}`);
-          this.dataSource.todayUsed = `${todayUsed}`;
-        } else {
-          this.dataSource.todayUsed = `0`;
-          result.push(`今日已用获取失败`);
-        }
-        let usedData = data.match(
-            /(Used Transfer|>过去已用|>已用|>总已用|\"已用)[^B]+/,
-        );
-        if (usedData) {
-          usedData = this.flowFormat(usedData[0]);
-          result.push(`已用：${usedData}`);
-          this.dataSource.usedData = `${usedData}`;
-        } else {
-          this.dataSource.usedData = `0`;
-          result.push(`累计使用获取失败`);
-        }
-        let restData = data.match(
-            /(Remaining Transfer|>剩余流量|>流量剩余|>可用|\"剩余)[^B]+/,
-        );
-        if (restData) {
-          restData = this.flowFormat(restData[0]);
-          result.push(`剩余：${restData}`);
-          this.dataSource.restData = `${restData}`;
-        } else {
-          this.dataSource.restData = `0`;
-          result.push(`剩余流量获取失败`);
-        }
-        resultData = result.join('\n');
-      }
-      console.log(resultData);
-    }
-  }
-
-  flowFormat(data) {
-    data = data.replace(/\d+(\.\d+)*%/, '');
-    let flow = data.match(/\d+(\.\d+)*\w*/);
-    return flow[0];
+    const webView = new WebView();
+    await webView.loadURL(url);
+    const js = `
+var response = {todayUsed: "0KB", usedData: "0KB", restData: "0KB"};
+if($('.progressbar').length){
+  response.todayUsed = $('.progressbar .label-flex:eq(0) .card-tag').text();;
+  response.usedData =  $('.progressbar .label-flex:eq(1) .card-tag').text();;
+  response.restData =  $('.progressbar .label-flex:eq(2) .card-tag').text();;
+} else if($('.card.card-statistic-2').length){
+  response.todayUsed =  $('.card.card-statistic-2:eq(1) .breadcrumb-item').text().split(' ')[1];
+  response.restData =  $('.card.card-statistic-2:eq(1) .card-body').text().trim();
+  response.usedData = document.body.innerHTML.match(/trafficDountChat\\s*\\(([^\\)]+)/)[1].match(/\\d[^\\']+/g)[0];
+}
+completion(response);
+    `;
+    const response = await webView.evaluateJavaScript(js, true);
+    this.dataSource = {...this.dataSource, ...response};
   }
 
   translateFlow(value) {
