221bb551ac1fa3d933bc86e37cf6ded14ea6f8a0
dompling <374779789@qq.com>
2021-01-20 11:35:49 +0800

    1

--- Scripts/DmYY.js
+++ Scripts/DmYY.js
@@ -1,3 +1,7 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: teal; icon-glyph: cogs;
+
 /*
  * Author: 2Ya
  * Github: https://github.com/dompling
