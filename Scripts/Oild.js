475e5019f41ef87098a663a11d35087efb1e889c
dompling <domplingyaya@gmail.com>
2024-10-31 09:39:37 +0800

    update:调整油价逻辑

--- Scripts/Oild.js
+++ Scripts/Oild.js
@@ -4,7 +4,7 @@
 
 // 添加require，是为了vscode中可以正确引入包，以获得自动补全等功能
 if (typeof require === 'undefined') require = importModule;
-const {DmYY, Runing} = require('./DmYY');
+const { DmYY, Runing } = require('./DmYY');
 
 const enumConfig = {
   89: '汽油',
@@ -13,6 +13,43 @@ const enumConfig = {
   98: '汽油',
   0: '柴油',
 };
+
+const provinces = [
+  '北京',
+  '天津',
+  '上海',
+  '重庆',
+  '河北',
+  '山西',
+  '辽宁',
+  '吉林',
+  '黑龙江',
+  '江苏',
+  '浙江',
+  '安徽',
+  '福建',
+  '江西',
+  '山东',
+  '河南',
+  '湖北',
+  '湖南',
+  '广东',
+  '海南',
+  '四川',
+  '贵州',
+  '云南',
+  '陕西',
+  '甘肃',
+  '青海',
+  '台湾',
+  '内蒙古',
+  '广西',
+  '西藏',
+  '宁夏',
+  '新疆',
+  '香港',
+  '澳门',
+];
 
 const squareColor = '#8165AC';
 const processColor = [`#7517F8`, `#E323FF`];
@@ -23,21 +60,36 @@ const processBarBgColor = '#5A5A89';
 class Widget extends DmYY {
   constructor(arg) {
     super(arg);
-    config.runsInApp &&
-    this.registerAction(
-      '油价设置',
-      () => {
-        return this.setAlertInput('油价设置', '设置类型,多个英文逗号分割', {
-          oilNumber: '92,95,89,0',
-        });
-      },
-      {name: 'paperplane', color: '#722ed1'},
-    );
     this.en = 'oilWidget';
     this.name = '油价';
-    config.runsInApp && this.registerAction('基础设置', this.setWidgetConfig);
-    this.cacheName = this.md5(`dataSource_${this.en}`);
-    this.oilNumber = `${this.settings.oilNumber || '92'}`.split(',');
+    if (config.runsInApp) {
+      this.registerAction(
+        '油价设置',
+        () => {
+          return this.setAlertInput('油价设置', '设置类型,多个英文逗号分割', {
+            oilNumber: '92,95,89,0',
+          });
+        },
+        { name: 'oilcan', color: '#E64C57' }
+      );
+
+      this.registerAction({
+        icon: { name: 'location.circle', color: '#8BE39D' },
+        type: 'switch',
+        title: '自动定位',
+        val: 'location',
+      });
+
+      this.registerAction({
+        icon: { name: 'mappin.and.ellipse.circle', color: '#5BBFF6' },
+        type: 'select',
+        title: '油价省份',
+        val: 'province',
+        placeholder: '地区',
+        options: provinces,
+      });
+      this.registerAction('基础设置', this.setWidgetConfig);
+    }
   }
 
   dataSource = {
@@ -59,6 +111,23 @@ class Widget extends DmYY {
   };
 
   init = async () => {
+    try {
+      this.oilNumber = `${this.settings.oilNumber || '92'}`.split(',');
+      const oilNumber = [];
+      this.oilNumber.forEach((item) => {
+        if (item && ['92', '95', '89', '0'].includes(item)) {
+          oilNumber.push(item);
+        }
+      });
+      this.oilNumber = oilNumber;
+      if (!this.oilNumber.length)
+        return this.notify(this.name, '请设置油价型号：92,95,89,0');
+    } catch (error) {
+      return console.log('请设置正确的油价');
+    }
+    this.province = this.settings.province;
+    this.location = this.settings.location;
+
     if (this.settings.dataSource) {
       this.dataSource = this.settings.dataSource[0];
     } else {
@@ -76,19 +145,27 @@ class Widget extends DmYY {
   };
 
   getOilPrice = async () => {
-    const location = await Location.current();
-    const locationText = await Location.reverseGeocode(
-      location.latitude,
-      location.longitude,
-    );
-    const {administrativeArea = ''} = locationText[0] || {};
-    const filter = `(CITYNAME="${administrativeArea.replace('省', '')}")`;
+    if (this.location) {
+      const location = await Location.current();
+      const locationText = await Location.reverseGeocode(
+        location.latitude,
+        location.longitude
+      );
+      const { administrativeArea = '' } = locationText[0] || {};
+      this.province = administrativeArea.replace('省', '');
+      this.settings.province = this.province;
+      this.saveSettings(false);
+    }
+
+    if (!this.province) return this.notify(this.name, '请设置油价省份！！');
+    console.log(`当前省份：${this.province}`);
+    this.province = `(CITYNAME="${this.province}")`;
     const time = Date.now();
     const url = `https://datacenter-web.eastmoney.com/api/data/v1/get?reportName=RPTA_WEB_YJ_JH&columns=ALL&filter=${encodeURIComponent(
-      filter,
+      this.province
     )}&sortColumns=DIM_DATE&sortTypes=-1&pageNumber=1&pageSize=6&source=WEB&_=${time}`;
 
-    const options = {url};
+    const options = { url };
     const response = await this.$request.post(options);
     console.log(response);
     if (response.result) {
@@ -125,7 +202,8 @@ class Widget extends DmYY {
     topLStack.addSpacer();
     topLStack.bottomAlignContent();
     const oilPrice = (this.dataSource[`V${this.oilNumber[0]}`] || '').toFixed(
-      2);
+      2
+    );
     const timer = (this.dataSource.DIM_DATE.split(' ')[0] || '').split('-');
     const oilNumText = topLStack.addText(`${oilPrice}`);
     oilNumText.textColor = this.widgetColor;
@@ -135,7 +213,7 @@ class Widget extends DmYY {
 
     const oilStatus = this.dataSource[`ZDE${this.oilNumber[0]}`] > 0;
     const oilZdeImage = SFSymbol.named(
-      oilStatus ? 'arrow.up' : 'arrow.up',
+      oilStatus ? 'arrow.up' : 'arrow.up'
     ).image;
     topStack.addSpacer();
     const topRStack = topStack.addStack();
@@ -158,7 +236,8 @@ class Widget extends DmYY {
     const bottomStack = w.addStack();
     bottomStack.addSpacer();
     const rightText = bottomStack.addText(
-      `${this.oilNumber[0]}#${enumConfig[this.oilNumber[0]]}`);
+      `${this.oilNumber[0]}#${enumConfig[this.oilNumber[0]]}`
+    );
     oilNumText.textColor = this.widgetColor;
     rightText.font = Font.boldSystemFont(18);
     rightText.textOpacity = 0.3;
@@ -168,7 +247,6 @@ class Widget extends DmYY {
   };
 
   rowData = (w, oilNumber) => {
-
     const oilPrice = (this.dataSource[`V${oilNumber}`] || '').toFixed(2);
     const oilZde = (this.dataSource[`ZDE${oilNumber}`] || '').toFixed(2);
     const oilType = enumConfig[oilNumber] || '';
@@ -182,10 +260,11 @@ class Widget extends DmYY {
     oilNumberStack.borderWidth = 4;
     oilNumberStack.borderColor = new Color(squareColor);
     oilNumberStack.centerAlignContent();
-    this.provideText(
-      `${oilNumber}`, oilNumberStack,
-      {font: 'bold', size: 26, color: new Color(squareColor)},
-    );
+    this.provideText(`${oilNumber}`, oilNumberStack, {
+      font: 'bold',
+      size: 26,
+      color: new Color(squareColor),
+    });
 
     colStack.addSpacer(7);
 
@@ -193,17 +272,20 @@ class Widget extends DmYY {
     oilInfoStack.size = new Size(65, colSize.height);
     oilInfoStack.layoutVertically();
     oilInfoStack.addSpacer();
-    this.provideText(
-      `#${oilType}`, oilInfoStack,
-      {font: 'light', size: 12, color: this.widgetColor, opacity: 0.5},
-    );
+    this.provideText(`#${oilType}`, oilInfoStack, {
+      font: 'light',
+      size: 12,
+      color: this.widgetColor,
+      opacity: 0.5,
+    });
 
     oilInfoStack.addSpacer(2);
 
-    this.provideText(
-      `${oilPrice}`, oilInfoStack,
-      {font: 'medium', size: 18, color: this.widgetColor},
-    );
+    this.provideText(`${oilPrice}`, oilInfoStack, {
+      font: 'medium',
+      size: 18,
+      color: this.widgetColor,
+    });
     oilInfoStack.addSpacer();
 
     const processStack = colStack.addStack();
@@ -211,17 +293,16 @@ class Widget extends DmYY {
     const processVerWidth = 10;
 
     let maxCount = 0;
-    const oilHistory = this.settings.dataSource.map(
-      item => {
-        const value = (item[`ZDE${oilNumber}`] /
-          item[`QE${oilNumber}`]).toFixed(2) * 100;
-        if (maxCount < value) maxCount = value;
-        return value;
-      });
+    const oilHistory = this.settings.dataSource.map((item) => {
+      const value =
+        (item[`ZDE${oilNumber}`] / item[`QE${oilNumber}`]).toFixed(2) * 100;
+      if (maxCount < value) maxCount = value;
+      return value;
+    });
 
     maxCount = maxCount * 1.5;
 
-    oilHistory.forEach(item => {
+    oilHistory.forEach((item) => {
       const processItemStack = processStack.addStack();
       processItemStack.size = new Size(processVerWidth, colSize.height);
       processItemStack.cornerRadius = processVerWidth / 2;
@@ -232,7 +313,9 @@ class Widget extends DmYY {
       const itemBarStack = processItemStack.addStack();
       itemBarStack.cornerRadius = processItemStack.cornerRadius;
       itemBarStack.size = new Size(
-        processVerWidth, colSize.height * (Math.abs(item) / maxCount));
+        processVerWidth,
+        colSize.height * (Math.abs(item) / maxCount)
+      );
       itemBarStack.backgroundGradient = this.gradient(processColor);
       if (item < 0) processItemStack.addSpacer();
       processStack.addSpacer();
@@ -251,13 +334,14 @@ class Widget extends DmYY {
     const oilZdeValueStack = oilZdeStack.addStack();
     oilZdeValueStack.centerAlignContent();
     oilZdeValueStack.addSpacer();
-    this.provideText(
-      `${oilZde > 0 ? '+' : ''} ${oilZde}`, oilZdeValueStack,
-      {font: 'light', size: 14, color: this.widgetColor},
-    );
+    this.provideText(`${oilZde > 0 ? '+' : ''} ${oilZde}`, oilZdeValueStack, {
+      font: 'light',
+      size: 14,
+      color: this.widgetColor,
+    });
 
     const oilZdeImage = SFSymbol.named(
-      oilZde > 0 ? 'arrow.up' : 'arrow.down',
+      oilZde > 0 ? 'arrow.up' : 'arrow.down'
     ).image;
 
     oilZdeValueStack.addSpacer(10);
@@ -282,7 +366,7 @@ class Widget extends DmYY {
     const processBarStack = processBarBgStack.addStack();
 
     const linear = new LinearGradient();
-    linear.colors = processBarColor.map(item => new Color(item));
+    linear.colors = processBarColor.map((item) => new Color(item));
     linear.locations = [0, 0.5];
     linear.startPoint = new Point(0, 0);
     linear.endPoint = new Point(1, 1);
@@ -290,7 +374,9 @@ class Widget extends DmYY {
     processBarStack.backgroundGradient = linear;
     processBarStack.cornerRadius = oilZdeSize.height / 2;
     processBarStack.size = new Size(
-      parseInt(oilZdeSize.width * oilZdeValue), oilZdeSize.height);
+      parseInt(oilZdeSize.width * oilZdeValue),
+      oilZdeSize.height
+    );
 
     if (oilZde > 0) processBarBgStack.addSpacer();
   };
@@ -303,9 +389,9 @@ class Widget extends DmYY {
     stack.borderWidth = 1;
   };
 
-  gradient = (color, config = {locations: [0, 0.5]}) => {
+  gradient = (color, config = { locations: [0, 0.5] }) => {
     const linear = new LinearGradient();
-    linear.colors = color.map(item => new Color(item));
+    linear.colors = color.map((item) => new Color(item));
     linear.locations = config.locations;
     return linear;
   };
